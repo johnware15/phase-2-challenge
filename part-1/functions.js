67018f027b96d5719be887c03983bc263f3da436
John Ware <johnware@Johns-MacBook-Air.local>
2017-07-24 15:54:02 -0700

    all functions working with a few testing issues

---
+++ part-1/functions.js
@@ -0,0 +1,65 @@
+let date = new Date(2017, 5, 19);
+let christmas = new Date(2017, 11, 25);
+let monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+var month = function(date) {
+    return monthList[date.getMonth()];
+}
+console.log(month(date));
+
+var month = function(christmas) {
+  return monthList[christmas.getMonth()];
+}
+console.log(month(christmas));
+
+///////////////////////////////////////////////////////
+
+let reverseSentence = function(string) {
+  if (typeof(string) === 'string') {
+    return string.split(' ').reverse().join(' ')
+  } else {
+    return "Not what we're looking for. Try Again."
+  }
+}
+
+console.log(reverseSentence("This better work."));
+/////////////////////////////////
+let friend = {
+  name: 'Dominique',
+  age: 30,
+  phone: '555-555-5555'
+}
+
+var nameProps = function(friend) {
+  return Object.keys(friend).sort();
+}
+
+console.log(nameProps(friend));
+
+
+/////////////////////////
+let movies = ["Dunkirk", "Valerian", "Black Panther", "Thor: Ragnarok", "Justice League","Wonder Woman", "Death Note", 1826946860]
+
+var filterBetween = function(movies, min, max) {
+  if(typeof(min) === 'string' && typeof(max) === 'string') {
+    return movies.filter(function(word) {
+      return word > min && word < max;
+    });
+  } else {
+    return "We have a bug."
+  }
+  }
+console.log(filterBetween(movies, "Black Panther", "Dunkirk"));
+
+console.log(filterBetween(movies, "Death Note", 1826946860));
+///////////////////////////////////////
+
+module.exports = {
+  date: date,
+  month: month,
+  monthList: monthList,
+  christmas: christmas,
+  reverseSentence: reverseSentence,
+  nameProps: nameProps,
+  filterBetween: filterBetween
+}
---
+++ part-1/tests.js
@@ -0,0 +1,49 @@
+const date = require('./functions.js').date
+const month = require('./functions.js').month
+const monthList = require('./functions.js').monthList
+const christmas = require('./functions.js').christmas
+const reverseSentence = require('./functions.js').reverseSentence
+const nameProps = require('./functions.js').nameProps
+const filterBetween = require('./functions.js').filterBetween
+
+/////////////////////////
+
+let newDate = new Date(2017, 5, 19);
+month(date)
+console.assert(month(date) === "June", "not working")
+
+let newChristmas = new Date(2017, 11, 25);
+month(christmas)
+console.assert(month(christmas) === "December", "no presents")
+
+////////////////////////////
+
+reverseSentence()
+
+console.assert(reverseSentence("This better work.") === "work. better This", "Redo that reverseSentence function.")
+
+// console.assert(typeOf reverseSentence(9582198628) === 'string', "Plug in a string, not an integer.")
+
+////////////////////////////
+
+let friend = {
+  name: 'Dominique',
+  age: 30,
+  phone: '555-555-5555'
+}
+
+nameProps()
+
+console.assert(nameProps(friend) === ['age', 'name', 'phone'], 'Try the nameProps function again.')
+
+// console.assert(nameProps(foe) === ['age', 'name', 'phone'], 'It appears we have a failed test.')
+
+////////////////////////////////
+
+let movies = ["Dunkirk", "Valerian", "Black Panther", "Thor: Ragnarok", "Justice League","Wonder Woman", "Death Note", 1826946860]
+
+filterBetween()
+
+console.assert(filterBetween(movies, "Black Panther", "Dunkirk") === ["Death Note"], "It is not filtering.")
+
+console.assert(filterBetween(movies, 1826946860, "Power") === ["Arrow", "Gypsy", "Game of Thrones"], "We have failure.")
